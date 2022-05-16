05144888ec983ab646d5ca5a39e359766811d8fe
ckrudals <2004ckck@naver.com>
2022-05-16 17:25:37 +0900

    feat : 상태 호이스팅

--- app/src/main/java/com/example/compose_codlab/MainActivity.kt
+++ app/src/main/java/com/example/compose_codlab/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,25 +18,66 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeCodlabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    MyApp()
-                }
+                MyApp()
+
             }
         }
     }
 }
 
 @Composable
-fun MyApp(name: List<String> = listOf("g", "m")) {
-    Column(Modifier.padding(14.dp)) {
+fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = {
+            shouldShowOnboarding = false
+        })
+    } else {
+        Greetings()
+    }
+
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text(text = "Continue")
+            }
+        }
+    }
+
+}
+
+
+@Composable
+fun Greetings(name: List<String> = listOf("g", "m")) {
+    Column(Modifier.padding(vertical = 4.dp)) {
         for (i in name) {
             Greeting(name = i)
         }
     }
 
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeCodlabTheme {
+        OnboardingScreen {}
+    }
 }
 
 @Composable
@@ -87,7 +124,7 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeCodlabTheme {
-        Greeting("Android")
+        Greetings()
     }
 }
 

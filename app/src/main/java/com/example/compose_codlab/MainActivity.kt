7949ee87490a8e036b7a31fbc87ffff64f2654aa
ckrudals <2004ckck@naver.com>
2022-05-16 20:17:55 +0900

    feat : list animation

--- app/src/main/java/com/example/compose_codlab/MainActivity.kt
+++ app/src/main/java/com/example/compose_codlab/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.compose_codlab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -13,6 +17,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_codlab.ui.theme.ComposeCodlabTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.saveable.rememberSaveable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +34,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = {
@@ -65,6 +70,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
 
 @Composable
+@Preview
 fun Greetings(name: List<String> = List(1000) { "${it}" }) {
     LazyColumn(Modifier.padding(vertical = 4.dp)) {
         items(name) { name ->
@@ -87,10 +93,14 @@ fun OnboardingPreview() {
 fun Greeting(name: String) {
 
     // 상태 저장할 변수
-    val expanded = remember {
-        mutableStateOf(false)
-    }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     // 앱에 넣고자 하는 공통 기능을 처리한다.
     Surface(
         color = MaterialTheme.colors.primary,
@@ -103,7 +113,7 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "hello.")
                 Text(text = name)
@@ -111,9 +121,9 @@ fun Greeting(name: String) {
             OutlinedButton(
                 // 클릭하면 반대가 됨
 
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(text = if (expanded.value) "Show more" else "fuck")
+                Text(text = if (expanded) "Show more" else "Hello")
             }
 
 
